93933c26022f94c64f1fc9620152a1453f9ea0be
Ethan101yer <99740945+Ethan101yer@users.noreply.github.com>
2025-07-07 19:36:29 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,7 @@ export interface Context {
 
 export type ManifestWithConfig = Manifest & { config: ManifestConfig[] };
 
-export type Config = Partial<Record<CountryCode | 'includeExternalUrls' | 'mediaFlowProxyUrl' | 'mediaFlowProxyPassword', string>>;
+export type Config = Partial<Record<string, boolean | string>>;
 
 export enum CountryCode {
   de = 'de',
